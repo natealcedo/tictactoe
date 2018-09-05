b2f3e1f450a0f8bbaa12e874b1f6fc7d05b0c7b3
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2018-09-05 23:49:30 +0800

    Add a gameboard component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PlayerForm from "../PlayerForm";
+import GameBoard from "../GameBoard";
 import "./app.css";
 
 const App = () => (
   <div className="app">
     <h1 className="header">Tic Tac Toe</h1>
     <PlayerForm />
+    <GameBoard />
   </div>
 );
 
