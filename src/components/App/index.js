d2b2d3f371519f5e207c4fcb4d3c3d4550b68235
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2018-09-05 17:11:25 +0800

    Edit app.css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PlayerForm from "../PlayerForm";
 import "./app.css";
 
 const App = () => (
   <div className="app">
     <h1 className="header">Tic Tac Toe</h1>
+    <PlayerForm />
   </div>
 );
 
